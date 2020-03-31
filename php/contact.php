c2448df62f1d2d863fa821f54f33d0884257fa8d
MatsudaTakuya <matsuda.takuya41@gmail.com>
2020-03-31 19:59:55 +0900

    minor fix

--- php/contact.php
+++ php/contact.php
@@ -64,6 +64,9 @@ if($request_param['token'] === '1234567'){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>クリ★スタコーディング課題【初級編】</title>
   <meta name="description" content="">
+  <!-- 検索結果から除外する -->
+  <meta name="robots" content="none">
+  <!-- Favicon -->
   <link rel="apple-touch-icon" sizes="180x180" href="../image/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="../image/favicon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="../image/favicon/favicon-16x16.png">
