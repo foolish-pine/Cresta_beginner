f5c2b71bcb6ee4c689466d4266667ef31ad7184e
MatsudaTakuya <matsuda.takuya41@gmail.com>
2020-04-12 09:33:35 +0900

    some fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,32 @@
 /* ================================================================
-  jQuery
+  javascript
    ================================================================ */
 
-// ---------------------------------------------
-// ハンバーガーメニュー
-// ---------------------------------------------
-
-//ウィンドウのリサイズ後にリロードする
 $(function () {
-  var timer = false;
-  var prewidth = $(window).width();
-  $(window).resize(function () {
-    if (timer !== false) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(function () {
-      var nowWidth = $(window).width();
-      if (prewidth !== nowWidth) {
-        location.reload();
-      }
-      prewidth = nowWidth;
-    }, 200);
-  });
+  // ---------------------------------------------
+  // ハンバーガーメニュー
+  // ---------------------------------------------
+  var mq = window.matchMedia("screen and (max-width:767px)");
 
-  // ヘッダー
-  $(".p-header__menu").on("click", function () {
-    $(".p-header__menuLine").stop(true).toggleClass("active");
-    $(".p-header__nav").stop(true).fadeToggle();
-  });
+  function checkBreakPoint(mq) {
+    // 画面幅767px以下で有効
+    if (mq.matches) {
+      // メニューアイコン開閉有効
+      $(".p-header__menu").on("click", function () {
+        $(".p-header__menuLine").stop(true).toggleClass("active");
+        $(".p-header__nav").stop(true).fadeToggle();
+      });
 
-  $(".p-header__nav, .p-header__logo").on("click", function () {
-    if ($(".p-header__menuLine").hasClass("active")) {
-      $(".p-header__menuLine").stop(true).toggleClass("active");
-      $(".p-header__nav").stop(true).fadeToggle();
+      // ナビの余白クリックでメニュー開閉
+      $(".p-header__nav").on("click", function () {
+        if ($(".p-header__menuLine").hasClass("active")) {
+          $(".p-header__menuLine").stop(true).toggleClass("active");
+          $(".p-header__nav").stop(true).fadeToggle();
+        }
+      });
     }
-  });
+  }
+  checkBreakPoint(mq);
 
   // ---------------------------------------------
   // スムーススクロール（ページ内リンク）
@@ -45,7 +37,7 @@ $(function () {
       href = $(this).attr("href"),
       target = $(href == "#" || href == "" ? "html" : href),
       headerHeight = $(".p-header").outerHeight(),
-      position = target.offset().top - headerHeight;
+      position = target.offset().top - headerHeight; // ヘッダーの高さ分スクロール量を減らす
     $("html, body").animate({ scrollTop: position }, speed);
   });
 });
