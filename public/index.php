df82239fe9bc5f2e5ddf61e753bbc33360cd37ae
MatsudaTakuya <matsuda.takuya41@gmail.com>
2020-04-22 10:29:52 +0900

    add: スクロールフェードイン追加

--- public/index.php
+++ public/index.php
@@ -113,7 +113,7 @@ function spaceTrim ($str) {
               弊社では、リリース時もサポートさせて頂きます。プレスリリース用のサイトや動画制作を通して、サービスのブランディングを行わせて頂きます。
             </p>
           </div>
-          <div class="p-service__image">
+          <div class="p-service__image  js-scroll-fadein">
             <img src="img/service-image@2x.jpg" alt="">
           </div>
         </div>
@@ -127,7 +127,7 @@ function spaceTrim ($str) {
               弊社では、リリース後もサポートさせて頂きます。サービスはリリース後に様々な課題にぶつかります。そこでクライアント様と一緒に改善を行うことで、デザインの効果を最大化させます。
             </p>
           </div>
-          <div class="p-service__image">
+          <div class="p-service__image  js-scroll-fadein">
             <img src="img/service-image02@2x.jpg" alt="">
           </div>
         </div>
